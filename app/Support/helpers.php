cff6fd6a7833f7169b5e8e66d858907b36807e4f
Antonykl <antonyklevertong@gmail.com>
2025-09-04 10:27:48 -0300

    Criação de helper que verifica se é admin ou user comum

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 
 if(! function_exists('human_case')) {
     function human_case($string)
@@ -8,3 +9,30 @@ if(! function_exists('human_case')) {
         return Str::tittle(__(Str::snake(Str::studly($string)," ")));
     }
 }
+
+if(! function_exists('isUserLoggedIn')){
+    function isUserLoggedIn()
+    {
+        return Auth::guard('web')->check();
+    }
+}
+
+if(! function_exists('isAdminLoggedIn')){
+    function isAdminLoggedIn()
+    {
+        return Auth::guard('admin')->check();
+    }
+}
+
+if(! function_exists('getCurrentLoggedinUser')){
+    function getCurrentLoggedinUser(): \App\Models\User|\App\Models\Admin|null{
+        if(isUserLoggedIn()){
+            return Auth::guard('web')->user();
+        }
+        if(isAdminLoggedIn()){
+            return Auth::guard('admin')->user();
+        }
+        return null;
+    }
+}
+
